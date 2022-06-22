a553d3a51faf2382069cb3045f0df658810ca06b
turretDive <apangjun@gmail.com>
2022-06-23 01:21:12 +0800

    Finished Missiles

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -40,7 +40,7 @@ public class Bullet : MonoBehaviour
     {
         GameObject effectIns = (GameObject) Instantiate(impactEffect, transform.position, transform.rotation);
         
-        Destroy(effectIns, 2f);
+        Destroy(effectIns, 5f);
 
         if (explosionRadius > 0f)
         {
